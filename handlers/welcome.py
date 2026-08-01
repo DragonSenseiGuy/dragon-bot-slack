e66f877cfa9fa7d6ac283eb63b21d657eda956e6
DragonSenseiGuy <hey@adityan.dev>
2026-07-31 20:30:28 -0400

    feat: add ephemeral leave-group button when user is added to ping group

--- handlers/welcome.py
+++ handlers/welcome.py
@@ -1,6 +1,8 @@
 import logging
 import os
 
+import requests
+
 WELCOME_CHANNEL = os.getenv("WELCOME_CHANNEL")
 NOTIFY_USER = "U09H4M0523Z"
 PING_GROUP = "S0A3L9DHB8F"
@@ -41,8 +43,65 @@ def register(app):
             channel=channel_id,
             text=(
                 f"Welcome to Aditya tries to Code <@{user_id}>{inviter_mention}, <@{NOTIFY_USER}> get in here. "
-                "Also btw you have been added to the ping group aditya-squad, "
-                "you can leave if you want to (I don't ping often)"
+                "Also btw you have been added to the ping group aditya-squad "
+                "(I don't ping often)."
             ),
         )
         logger.info(f"Welcome message sent for <@{user_id}>")
+
+        client.chat_postEphemeral(
+            channel=channel_id,
+            user=user_id,
+            text="You've been added to the ping group aditya-squad.",
+            blocks=[
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "You've been added to the ping group *aditya-squad*. You can leave it anytime.",
+                    },
+                },
+                {
+                    "type": "actions",
+                    "elements": [
+                        {
+                            "type": "button",
+                            "text": {"type": "plain_text", "text": "Leave group"},
+                            "style": "danger",
+                            "action_id": "leave_ping_group",
+                            "value": user_id,
+                        }
+                    ],
+                },
+            ],
+        )
+
+    @app.action("leave_ping_group")
+    def handle_leave_ping_group(ack, body, client):
+        ack()
+        user_id = body["actions"][0]["value"]
+        response_url = body.get("response_url")
+
+        logger.info(f"<@{user_id}> requested to leave ping group {PING_GROUP}")
+
+        try:
+            current_users = client.usergroups_users_list(usergroup=PING_GROUP)
+            user_list = current_users.get("users", [])
+            if user_id in user_list:
+                user_list.remove(user_id)
+                client.usergroups_users_update(usergroup=PING_GROUP, users=user_list)
+                logger.info(f"Removed <@{user_id}> from ping group {PING_GROUP}")
+                result_text = "You've left the ping group aditya-squad."
+            else:
+                logger.debug(f"<@{user_id}> was not in ping group")
+                result_text = "You're not in the ping group aditya-squad."
+        except Exception as e:
+            logger.error(f"Failed to remove user from ping group: {e}")
+            result_text = ":x: Failed to leave the ping group. Please try again later."
+
+        if response_url:
+            requests.post(
+                response_url,
+                json={"text": result_text, "replace_original": True},
+                timeout=10,
+            )
